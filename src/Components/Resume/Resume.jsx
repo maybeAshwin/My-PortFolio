76fc1ce1237e36cc407f17b577efe2a12ddb881b
Ashwin Joseph <ashwinjoseph2017@gmail.com>
2024-08-08 01:33:12 +0530

    Resume page updated and some pages added and design updated

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -20,21 +20,35 @@ const Resume = () => {
             development. Proficient in using Flutter ,Dart,
             HTML, JavaScript, CSS and React to develop
             effective applications according to client
-            requirements.
-            </p>
+            requirements.</p>
+            <br />
+            <div className="divider"></div>
+            <br />
+            
             <h2>Skills</h2>
-            <li>React</li>
-            <li>Flutter</li>
-            <li>Web Devlopment</li>
-            <li>Dart</li>
-
+              <li>React</li>
+              <li>Flutter</li>
+              <li>Web Devlopment</li>
+              <li>Dart</li>
+            <br />
+            <div className="divider"></div>
+            <br />
             <h2>Education</h2>
-            <li><h3>RIT Kottyam</h3><br /><p>2022-2025 <br />B.Tech <br />Kottayam,Kerala</p></li>
-            <li><h3>GPTC PALA</h3><br /><p>2019-2022 <br />DIploma In Computer Engineering <br />Kottayam,Kerala</p></li>
-            <li><h3>OLL HSS Uzhavoor</h3><br /><p>2017-2019 <br />Higher Secondary <br />Kottayam,Kerala</p></li>
+            <div className="education">
+              
+                <li><h3>RIT KOTTAYAM</h3><br /><p>2022-2025 <br />B.Tech <br />Kottayam,Kerala</p></li>
+                <li><h3>GPTC PALA</h3><br /><p>2019-2022 <br />DIploma In Computer<br />Engineering <br />Kottayam,Kerala</p></li>
+                <li><h3>OLL HSS UZHAVOOR</h3><br /><p>2017-2019 <br />Higher Secondary <br />Kottayam,Kerala</p></li>
+                <li><h3>GHSS EDAKKOLY</h3><br /><p>2007-2017 <br />High School <br />Kottayam,Kerala</p></li>
+            </div>
+
+            <h2>Social Links</h2>
+              <li>img</li>
+
 
         </div>
-        <div className="divider"></div>
+        
+
         
        </div>
     </div>
